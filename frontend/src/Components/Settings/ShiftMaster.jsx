90853e3f7fcea38116cc503cdf963b1414334fa1
Priyanshu Lapkale <66089621+mh31-priyanshu@users.noreply.github.com>
2024-09-28 01:38:32 +0530

    updated login.jsx

--- frontend/src/Components/Settings/ShiftMaster.jsx
+++ frontend/src/Components/Settings/ShiftMaster.jsx
@@ -176,9 +176,10 @@ export default function ShiftMaster() {
               onChange={(e) => setShiftName(e.target.value)}
             />
           </div>
-          <label class="inline-flex items-center me-5 cursor-pointer">
+          <label class="block text-sm font-medium text-gray-700 mb-1">
+            Active
             <input type="checkbox" value="false" class="sr-only peer"  />
-            <div class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+            <div class="relative w-11 h-6 px-6 bg-gray-200 rounded-full peer dark:bg-gray-700  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
           </label>
           <div>
             <label htmlFor="start-time" className="block text-sm font-medium text-gray-700 mb-1">Start Time</label>
