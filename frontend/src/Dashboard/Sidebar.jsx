51b2226eb48bda3858421d16b0d9527060eb54a2
Priyanshu Lapkale <66089621+mh31-priyanshu@users.noreply.github.com>
2024-09-28 02:04:20 +0530

    updated login.jsx

--- frontend/src/Dashboard/Sidebar.jsx
+++ frontend/src/Dashboard/Sidebar.jsx
@@ -25,7 +25,7 @@ function Sidebar() {
 
     const handleLogout = async () => {
         try {
-          const response = await fetch('http://localhost:5000/user/logout', {
+          const response = await fetch('https://employee-tracebility-backend.vercel.app/user/logout', {
             method: 'GET', // or 'GET', depending on your API
             credentials: 'include', // Include cookies if necessary
             headers: {
